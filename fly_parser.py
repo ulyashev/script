64f89a3dca4b9f1bd60421115860f3c7688f2c08
ulyashev <alexeyulyashev@gmail.com>
2017-07-02 09:48:36 +0300

    Добавлена сортировка вывода маршрутов "в один конец".

--- fly_parser.py
+++ fly_parser.py
@@ -19,12 +19,9 @@ def valid_date(inp_date):
     except ValueError:
         print 'Введите корректную дату'
         return False
-
 def parser_flyniki(iata_depart, iata_destination, out_data, return_data):
-
     oneway = '' if return_data else  'on'
     start_url = 'https://www.flyniki.com/ru/start.php'
-
     headers = { 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:50.0) Gecko/20100101 Firefox/50.0',
                 'Referer': 'https://www.flyniki.com/ru/start.php',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -36,10 +33,8 @@ def parser_flyniki(iata_depart, iata_destination, out_data, return_data):
             'bookingmask_widget_id': 'bookingmask-widget-stageoffer',
             'bookingmask_widget_dateformat': 'dd.mm.yy',
             'oneway': 'on',}
-
     req_sess = requests.Session()
     start_post = req_sess.post(start_url, data=data, headers=headers, verify=False)
-    
     data_res = [('_ajax[templates][]', 'main'),
                 ('_ajax[templates][]', 'priceoverview'),
                 ('_ajax[templates][]', 'infos'),
@@ -55,7 +50,6 @@ def parser_flyniki(iata_depart, iata_destination, out_data, return_data):
                 ('_ajax[requestParams][infantCount]', '0'),
                 ('_ajax[requestParams][openDateOverview]', ''),
                 ('_ajax[requestParams][oneway]', oneway),]
-    
     headers_res = { 'Accept': 'application/json, text/javascript, */*',
                     'Host': 'www.flyniki.com',
                     'Origin': 'https://www.flyniki.com',
@@ -96,7 +90,7 @@ def parser_flyniki(iata_depart, iata_destination, out_data, return_data):
     price_return = parser_fly_html('.//*[@class="return block"]/div[2]/table/tbody/')
     if not return_data:
         print 'Варианты маршрутов:'
-        for elem_out in price_outbond:
+        for elem_out in sorted(price_outbond, key=lambda x: x[-1]):
             print 'Вылет:{}, прибытие: {}, длительность:{}, класс:{}, стоимость: {}'.format(*elem_out) + currency
     else:
         print 'Варианты маршрутов:'
@@ -106,24 +100,18 @@ def parser_flyniki(iata_depart, iata_destination, out_data, return_data):
                 print 'Вылет:{}, прибытие: {}, длительность:{}, класс:{}, стоимость: {}'.format(*elem_ret) + currency
                 print 'Общая стоимость: ', elem_out[-1] + elem_ret[-1], currency
 def parser():
-    
     while True:
         out_data = raw_input('Введите дату вылета (yyyy-mm-dd): ')
         if valid_date(out_data):
             break
     while True:
-        return_data = raw_input('Введите дату возвращения (yyyy-mm-dd): ')
+        return_data = raw_input('Введите дату возвращения или Enter (если билет в одну сторону) (yyyy-mm-dd): ')
         if return_data:
             if valid_date(return_data):
                 break
         return_data  =''
         break
-    iata_depart = raw_input('Введите аэропорт вылета: (IATA)')
-    iata_destination = raw_input('Введите аэропорт назначения: (IATA)')
+    iata_depart = raw_input('Введите аэропорт вылета (IATA): ')
+    iata_destination = raw_input('Введите аэропорт назначения (IATA): ')
     parser_flyniki(iata_depart, iata_destination, out_data, return_data)
 parser()
-
-
-
-
-
