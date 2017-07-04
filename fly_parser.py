4e2035f913dc43dd55e736fc081b0825d944063a
ulyashev <alexeyulyashev@gmail.com>
2017-07-04 21:52:08 +0300

    Внесены изменения в соответствии с рецензией.

--- fly_parser.py
+++ fly_parser.py
@@ -6,14 +6,17 @@
 поиск возможных вариантов перелета и вывод информации на экран."""
 import sys
 from datetime import datetime, timedelta
+from itertools import product
 import requests
 from lxml import html
+
 
 
 def valid_date(date):
     """ Проверка валидности даты."""
     try:
-        if datetime.strptime(date, '%Y-%m-%d') - datetime.now() < timedelta(-1):
+        if (datetime.strptime(date, '%Y-%m-%d') -
+                datetime.now()) < timedelta(-1):
             print 'Дата меньше текущей.'
             return
         return True
@@ -26,16 +29,18 @@ def parser_fly_html(fly_html, path_x):
     """ Функция производит разбор ответа, полученного от сервера
     и формирование результатов обработки для вывода на экран."""
     price = list()
-    for row in range(len(fly_html.xpath(path_x + "tr"))):
-        block = fly_html.xpath(path_x + 'tr[' + str(row) + ']/td')
+    for row in range(len(fly_html.xpath(path_x + 'tr'))):
+        block = fly_html.xpath('{}tr[{}]/td'.format(path_x, row))
         for node in block:
             res = node.xpath('.//*[@class="lowest"]/span/@title')
             for elem in res:
-                price.append(list([elem.split(',')[1][:6],
-                                   elem.split(',')[1][7:],
-                                   elem.split(',')[2],
-                                   elem.split(',')[3].split(':')[0],
-                                   float(''.join(elem.split(':')[3].split('.')).replace(',', '.'))]))
+                price.append([
+                    elem.split(',')[1][:6],
+                    elem.split(',')[1][7:],
+                    elem.split(',')[2],
+                    elem.split(',')[3].split(':')[0],
+                    float(''.join(elem.split(':')[3].split('.')).replace(',', '.'))
+                ])
     return price
 
 
@@ -47,19 +52,17 @@ def info_output(price_outbond, price_return, currency, return_date):
                    ' стоимость: {}').format(*elem_out) + currency
             print '--------------------------------------------------------'
     else:
-        price_result = list()
-        for elem_out in price_outbond:
-            for elem_ret in price_return:
-                price_result.append({'track_out': elem_out,
-                                     'track_return': elem_ret,
-                                     'total_sum': elem_out[-1] + elem_ret[-1]})
+        price_result = []
+        for elem_out, elem_ret in product(price_outbond, price_return):
+            price_result.append({'track_out': elem_out,
+                                 'track_return': elem_ret,
+                                 'total_sum': elem_out[-1] + elem_ret[-1]})
         for elem_res in sorted(price_result, key=lambda x: x['total_sum']):
-            print ('Вылет:{}, прибытие: {}, длительность:{}, класс:{},' +\
+            print ('Вылет:{}, прибытие: {}, длительность:{}, класс:{},'
                    'стоимость:{}').format(*elem_res['track_out']) + currency
-            print ('Вылет:{}, прибытие: {}, длительность:{}, класс:{},' +\
+            print ('Вылет:{}, прибытие: {}, длительность:{}, класс:{},'
                    'стоимость:{}').format(*elem_res['track_return']) + currency
-            print 'Общая стоимость: ', elem_res['track_out'][-1] + \
-                elem_res['track_return'][-1], currency
+            print 'Общая стоимость: ', elem_res['total_sum'], currency
             print '---------------------------------------------------------'
 
 
@@ -67,68 +70,87 @@ def parser_flyniki(iata_depart, iata_destination, out_date, return_date):
     """ Функция из полученных данных формирует и отрпавляет запрос."""
     oneway = '' if return_date else 'on'
     start_url = 'https://www.flyniki.com/ru/start.php'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; ' +
-                             'rv:50.0) Gecko/20100101 Firefox/50.0',
-               'Referer': 'https://www.flyniki.com/ru/start.php',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;' +
-                         'q=0.9,*/*;q=0.8',
-               'Accept-Language': 'ru,en-US;q=0.7,en;q=0.3',
-               'Accept-Encoding': 'gzip, deflate, br',
-               'Connection': 'keep-alive'}
-    data = {'market': 'RU',
-            'language': 'ru',
-            'bookingmask_widget_id': 'bookingmask-widget-stageoffer',
-            'bookingmask_widget_dateformat': 'dd.mm.yy',
-            'oneway': 'on'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:50.0) Gecko/20100101 Firefox/50.0',
+        'Referer': 'https://www.flyniki.com/ru/start.php',
+        'Accept': 'text/html,application/xhtml+xml,application/xml; q=0.9,*/*;q=0.8',
+        'Accept-Language': 'ru,en-US;q=0.7,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+    }
+    data = {
+        'market': 'RU',
+        'language': 'ru',
+        'bookingmask_widget_id': 'bookingmask-widget-stageoffer',
+        'bookingmask_widget_dateformat': 'dd.mm.yy',
+        'oneway': oneway
+    }
     req_sess = requests.Session()
-    start_post = req_sess.post(start_url, data=data,
-                               headers=headers, verify=False)
-    data_res = [('_ajax[templates][]', 'main'),
-                ('_ajax[templates][]', 'priceoverview'),
-                ('_ajax[templates][]', 'infos'),
-                ('_ajax[templates][]', 'flightinfo'),
-                ('_ajax[requestParams][departure]', iata_depart),
-                ('_ajax[requestParams][destination]', iata_destination),
-                ('_ajax[requestParams][returnDeparture]', ''),
-                ('_ajax[requestParams][returnDestination]', ''),
-                ('_ajax[requestParams][outboundDate]', out_date),
-                ('_ajax[requestParams][returnDate]', return_date),
-                ('_ajax[requestParams][adultCount]', '1'),
-                ('_ajax[requestParams][childCount]', '0'),
-                ('_ajax[requestParams][infantCount]', '0'),
-                ('_ajax[requestParams][openDateOverview]', ''),
-                ('_ajax[requestParams][oneway]', oneway)]
-    headers_res = {'Accept': 'application/json, text/javascript, */*',
-                   'Host': 'www.flyniki.com',
-                   'Origin': 'https://www.flyniki.com',
-                   'Accept-Encoding': 'gzip, deflate',
-                   'Accept-Language': 'en-US,en;q=0.8',
-                   'Content-Type': 'application/x-www-form-urlencoded',
-                   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)' +
-                                 ' AppleWebKit/537.36 (KHTML, like Gecko) ' +
-                                 'Chrome/54.0.2840.59 Safari/537.36',
-                   'X-Requested-With': 'XMLHttpRequest'}
-    result = req_sess.post(start_post.url, data=data_res, headers=headers_res, verify=False)
+    start_post = req_sess.post(
+        start_url,
+        data=data,
+        headers=headers,
+        verify=False
+    )
+    data_res = [
+        ('_ajax[templates][]', 'main'),
+        ('_ajax[templates][]', 'priceoverview'),
+        ('_ajax[templates][]', 'infos'),
+        ('_ajax[templates][]', 'flightinfo'),
+        ('_ajax[requestParams][departure]', iata_depart),
+        ('_ajax[requestParams][destination]', iata_destination),
+        ('_ajax[requestParams][returnDeparture]', ''),
+        ('_ajax[requestParams][returnDestination]', ''),
+        ('_ajax[requestParams][outboundDate]', out_date),
+        ('_ajax[requestParams][returnDate]', return_date),
+        ('_ajax[requestParams][adultCount]', '1'),
+        ('_ajax[requestParams][childCount]', '0'),
+        ('_ajax[requestParams][infantCount]', '0'),
+        ('_ajax[requestParams][openDateOverview]', ''),
+        ('_ajax[requestParams][oneway]', oneway)
+    ]
+    headers_res = {
+        'Accept': 'application/json, text/javascript, */*',
+        'Host': 'www.flyniki.com',
+        'Origin': 'https://www.flyniki.com',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    result = req_sess.post(
+        start_post.url,
+        data=data_res,
+        headers=headers_res,
+        verify=False
+    )
     try:
-        fly_html = html.fromstring(result.json()['templates']['main'])
+        res_json = result.json()
+        fly_html = html.fromstring(res_json['templates']['main'])
     except KeyError:
-        if result.json()['errorRAW'][0]['code'] == 'departure':
+        if res_json['errorRAW'][0]['code'] == 'departure':
             print 'Введен не кооректный IATA аэропорта отправления.'
             return
-        if result.json()['errorRAW'][0]['code'] == 'destination':
+        if res_json['errorRAW'][0]['code'] == 'destination':
             print 'Введен не кооректный IATA аэропорта назначения.'
             return
-    if not bool(result.json()['templates']['priceoverview']):
-        print 'Не удалось найти рейсы на запрошенную дату.'
+    if not res_json['templates']['priceoverview']:
+        print 'Не удалось найти рейсы на запрошенную дату(ы).'
         return
-    currency = fly_html.xpath('.//*[@id="flighttables"]/div[1]/div[2]/' +
-                              'table/thead/tr[2]/th[4]/text()')[0]
-
-    price_outbond = parser_fly_html(fly_html, './/*[@class="outbound block"]/' +
-                                    'div[2]/table/tbody/')
-    price_return = parser_fly_html(fly_html, './/*[@class="return block"]/' +
-                                   'div[2]/table/tbody/')
+    currency = fly_html.xpath(
+        './/*[@id="flighttables"]/div[1]/div[2]/'
+        'table/thead/tr[2]/th[4]/text()')[0]
+    price_outbond = parser_fly_html(
+        fly_html,
+        './/*[@class="outbound block"]/div[2]/table/tbody/'
+    )
+    price_return = parser_fly_html(
+        fly_html,
+        './/*[@class="return block"]/div[2]/table/tbody/'
+    )
     info_output(price_outbond, price_return, currency, return_date)
+
 
 def parser(args):
     """ Главная функция."""
@@ -146,11 +168,11 @@ def parser(args):
     if return_date:
         if not valid_date(return_date):
             return
-        if datetime.strptime(return_date, '%Y-%m-%d') - datetime.strptime(out_date, '%Y-%m-%d') < timedelta():
+        if (datetime.strptime(return_date, '%Y-%m-%d') -
+                datetime.strptime(out_date, '%Y-%m-%d')) < timedelta():
             print 'Дата возвращения меньше даты вылета'
             return
     parser_flyniki(iata_depart, iata_destination, out_date, return_date)
 
+
 parser(sys.argv)
-
-
